9eed5d94bfc968286018e35cfd18cf8427414476
mamariscal <mamariscal@csumb.edu>
2019-07-06 23:47:33 -0400

    fix

--- js/hangman.js
+++ js/hangman.js
@@ -5,49 +5,48 @@ var remainingGuesses = 6;
 var words = [{word: "snake", hint: "It's a reptile."},
              {word: "monkey", hint: "It's a mammal."},
              {word: "beetle", hint: "It's a insect."}];
-
-  
 var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G',
   'H', 'I', 'J', 'K', 'L', 'M', 'N',
   'O', 'P', 'Q', 'R', 'S', 'T', 'U',
   'V', 'W', 'X', 'Y', 'Z'
 ]
 
-//LISTENERS
+// listeners
 window.onload = startGame();
-$(".replayBtn").on("click", function() {
-  location.reload();
-});
 $(".letter").click(function(){
   checkLetter($(this).attr("id"));
   disableButton($(this));
   console.log($(this).attr("id"));
 });
+
+$(".replayBtn").on("click", function() {
+  location.reload();
+  
+});
+
 $(".helpBtn").click(function() {
   disableButton($(this));
   showHint();
 });
-  
-//FUNCTION
-function startGame(){ 
+
+// functions
+function startGame() {
   pickWord();
   initBoard();
   createLetters();
   updateBoard();
-
 }
 
 function initBoard() {
   for (var letter in selectedWord) {
-    board.push("_")
+    board.push("_");
   }
 }
 
-
 function pickWord() {
   var randomInt = Math.floor(Math.random() * words.length);
-  selectedWord = words[randomInt].toUpperCase();
-  selectedHint = word[randomInt].hint;
+  selectedWord = words[randomInt].word.toUpperCase();
+  selectedHint = words[randomInt].hint;
 }
 
 function updateBoard() {
@@ -58,62 +57,20 @@ function updateBoard() {
   
 }
 
-
-
-//Creates the letters inside the letter div
-function createLetters() {
-  for ( var letter of alphabet){
-    $("#letters").append("<button class = 'letter' id='" + letter + "'>" + letter + "</button>");
-  }
-}
-//Checks to see if the selected letter exists in the selectedWord
-
-function checkLetter(letter) {
-  var position = [];
-
-  //Put all the position the letter exists in an array
-  for (var i = 0; i < selectedWord.length; i++) {
-    console.log(selectedWord)
-    if (letter == selectedWord[i]) {
-      positions.push(i);
-    }
-  }
-
-  if (positions.length > 0) {
-    updateWord(positions, letter);
-    
-    if(!board.includes('_')){
-      endGame(true);
-    }
-  } else {
-    remainingGuesses -= 1;
-    updateMan();
-  }
-if(remainingGuesses <= 0){
-  endGame(false)  
-  }
-}
-
-function updateWord(position, letter) {
-  for (var pos of positions) {
-    board[pos] = letter;
-  }
-  updateBoard();
-}
-
 function updateMan() {
   $("#hangImg").attr("src", "img/stick_" + (6 - remainingGuesses) + ".png");
 }
 
-function endGame(win) {
+function endGame(win) // win is boolean
+{
   $("#letters").hide();
-
   if (win) {
     $('#won').show();
   } else {
     $('#lost').show();
   }
 }
+
 function showHint(){
   console.log("ShowHint()");
   remainingGuesses -= 1;
@@ -121,10 +78,52 @@ function showHint(){
   $("#word").append("<br />");
   $("#word").append("<span class='hint'>Hint: " + selectedHint + "</span>");
 }
+
+function createLetters() {
+  for (var letter of alphabet) {
+    $("#letters").append("<button class='letter' id='" + letter + "'>" + letter + "</button>");
+  }
+}
+//checks to see if selected letter exists in the array
+function checkLetter(letter) {
+  console.log(letter);
+  var positions = [];
+  
+  for (var i = 0; i < selectedWord.length; i++) {
+    if (letter == selectedWord[i]) {
+      positions.push(i);
+    }
+  }
+  
+  if(positions.length > 0) {
+    updateWord(positions, letter);
+    
+    if(!board.includes('_')) {
+      endGame(true);
+    }
+    
+  } else {
+    remainingGuesses -= 1;
+    updateMan();
+  }
+  
+  if(remainingGuesses <= 0) {
+    endGame(false);
+  }
+}
+// update current word then call for board update
+function updateWord(positions, letter) {
+  for (var pos of positions) {
+    board[pos] = letter;
+  }
+  updateBoard();
+}
+
 function disableButton(btn) {
   btn.prop("disabled", true);
-  btn.attr("class", "btn btn-danger")
+  btn.attr("class", "btn btn-danger");
 }
+
 function enableButton(btn) {
   btn.prop("enabled", true);
   btn.attr("class", "btn btn-success");
